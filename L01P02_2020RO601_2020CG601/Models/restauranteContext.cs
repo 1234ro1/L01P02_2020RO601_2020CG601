b6b4e5762ddb0c69dc49571047aa6071c15bf965
1234ro1 <josue.ramirez@catolica.edu.sv>
2023-04-22 09:06:17 -0600

    laboratorio

--- L01P02_2020RO601_2020CG601/Models/restauranteContext.cs
+++ L01P02_2020RO601_2020CG601/Models/restauranteContext.cs
@@ -1,6 +1,10 @@
-﻿namespace L01P02_2020RO601_2020CG601.Models
+﻿using Microsoft.EntityFrameworkCore;
+namespace L01P02_2020RO601_2020CG601.Models
 {
-    public class restauranteContext
+    public class restauranteContext : DbContext
     {
+        public restauranteContext(DbContextOptions options) : base(options)
+        {
+        }
     }
 }
