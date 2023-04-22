1cd29b346dd625dfce9ebd1ea748493d81fa1e58
1234ro1 <josue.ramirez@catolica.edu.sv>
2023-04-22 09:23:18 -0600

    laboratorio

--- L01P02_2020RO601_2020CG601/Models/pedidos.cs
+++ L01P02_2020RO601_2020CG601/Models/pedidos.cs
@@ -1,6 +1,19 @@
-﻿namespace L01P02_2020RO601_2020CG601.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P02_2020RO601_2020CG601.Models
 {
     public class pedidos
     {
+        [Key]
+        public int pedidoId { get; set; }
+        public int motoristaId { get; set; }
+        public int clienteId { get; set; }
+        public int platoId { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+
+
+
+
+
     }
 }
